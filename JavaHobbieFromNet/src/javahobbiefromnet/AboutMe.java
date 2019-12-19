2e806538320309f0b3757ef26722f1c0ded91caa
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 15:17:12 +0200

    author labeling

--- JavaHobbieFromNet/src/javahobbiefromnet/AboutMe.java
+++ JavaHobbieFromNet/src/javahobbiefromnet/AboutMe.java
@@ -1,5 +1,8 @@
 package javahobbiefromnet;
-
+/**
+ *
+ * @author Filipe Mangue
+ */
 public class AboutMe {
     public static void main(String[] args) {
         System.out.println("###########  ##  ##        ##  #######    #########");

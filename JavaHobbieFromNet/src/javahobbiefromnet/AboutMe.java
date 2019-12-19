a8c8873198c4e1408ad868337788094e2adeb18f
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 15:09:42 +0200

    print my own first name

--- JavaHobbieFromNet/src/javahobbiefromnet/AboutMe.java
+++ JavaHobbieFromNet/src/javahobbiefromnet/AboutMe.java
@@ -2,6 +2,13 @@ package javahobbiefromnet;
 
 public class AboutMe {
     public static void main(String[] args) {
-        
+        System.out.println("###########  ##  ##        ##  #######    #########");
+        System.out.println("##           ##  ##        ##  ##     #   ##");
+        System.out.println("##           ##  ##        ##  ##      #  ##");
+        System.out.println("#######      ##  ##        ##  ########   #########");
+        System.out.println("##           ##  ##        ##  ##         ##");
+        System.out.println("##           ##  ##        ##  ##         ##");
+        System.out.println("##           ##  ##        ##  ##         ##");
+        System.out.println("##           ##  ########  ##  ##         #########");
     }
 }

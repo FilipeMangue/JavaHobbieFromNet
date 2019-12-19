686830cfb46464f0d1a81ef5520f536e1f94105b
FilipeEmanuel <filipeemanuelmangue@gmail.com>
2019-12-19 15:26:35 +0200

    moz flag

--- JavaHobbieFromNet/src/javahobbiefromnet/MozambicanFlag.java
+++ JavaHobbieFromNet/src/javahobbiefromnet/MozambicanFlag.java
@@ -5,5 +5,21 @@ package javahobbiefromnet;
  * @author Filipe Mangue
  */
 public class MozambicanFlag {
-    
+    public static void main(String[] args) {
+        System.out.println("* ============================================");                          
+        System.out.println("* * ==========================================");                           
+        System.out.println("* * * ========================================");                           
+        System.out.println("* * * * ======================================");                           
+        System.out.println("* * * * * ####################################");                          
+        System.out.println("* * * * * * ==================================");                          
+        System.out.println("* * * * * * * ================================");                          
+        System.out.println("* * * * * * * * ==============================");                          
+        System.out.println("* * * * * * * ================================");                          
+        System.out.println("* * * * * * ==================================");                           
+        System.out.println("* * * * * ####################################");                          
+        System.out.println("* * * * ======================================");                          
+        System.out.println("* * * ========================================");                          
+        System.out.println("* * ==========================================");                          
+        System.out.println("* ============================================");
+    }
 }
